dddf2f6b92a92a9fcd746e607faad8a3ad5fcfb4
MyNameIsTaeYeong <imtaebari@gmail.com>
2021-05-07 10:59:01 +0900

    이전달,다음달 버튼구현

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,26 +3,53 @@ import Day from "./Day";
 import "./Calendar.css";
 
 class Calendar extends React.Component {
-  state = {};
-
   constructor(props) {
     super(props);
     this.leapYearMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     this.notLeapYearMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    this.state = {
+      year: new Date().getFullYear(),
+      month: new Date().getMonth(),
+    };
+
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
   }
 
-  componentDidMount() {}
+  next() {
+    if (this.state.month === 11) {
+      this.setState((state) => ({
+        month: 0,
+        year: state.year + 1,
+      }));
+    } else {
+      this.setState((state) => ({
+        month: state.month + 1,
+      }));
+    }
+  }
 
-  componentDidUpdate() {}
-  componentWillUnmount() {}
+  prev() {
+    if (this.state.month === 0) {
+      this.setState((state) => ({
+        month: 11,
+        year: state.year - 1,
+      }));
+    } else {
+      this.setState((state) => ({
+        month: state.month - 1,
+      }));
+    }
+  }
 
   render() {
-    if (this.props.year % 4 === 0) {
-      this.lastDay = this.leapYearMonth[this.props.month];
+    if (this.state.year % 4 === 0) {
+      this.lastDay = this.leapYearMonth[this.state.month];
     } else {
-      this.lastDay = this.notLeapYearMonth[this.props.month];
+      this.lastDay = this.notLeapYearMonth[this.state.month];
     }
-    this.firstDay = new Date(this.props.year, this.props.month, 1).getDay();
+    this.firstDay = new Date(this.state.year, this.state.month, 1).getDay();
 
     this.rows = [];
     this.row = [];
@@ -49,8 +76,14 @@ class Calendar extends React.Component {
       <div>
         <div className="calendar">
           <div className="calendar__row">
+            <div>
+              <button onClick={this.prev}>prev</button>
+            </div>
             <div className="calendar__row--title">
-              {this.props.year + "년 " + (this.props.month + 1) + "월"}
+              {this.state.year + "년 " + (this.state.month + 1) + "월"}
+            </div>
+            <div>
+              <button onClick={this.next}>next</button>
             </div>
           </div>
           <div className="calendar__row">
