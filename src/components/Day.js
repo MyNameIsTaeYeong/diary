be2d122272f154db294beb47811a9416ba0dbc40
MyNameIsTaeYeong <imtaebari@gmail.com>
2021-05-10 22:08:51 +0900

    modal창 꾸미기

--- src/components/Day.js
+++ src/components/Day.js
@@ -4,9 +4,16 @@ function Day({ openModal, closeModal, modalCheck, date }) {
   return (
     <div>
       {modalCheck ? (
-        <section className="modal">
-          <button onClick={closeModal}>x</button>
-        </section>
+        <div className="modal">
+          <section>
+            <header>
+              <span>2020 5월 20일</span>
+              <button onClick={closeModal}>x</button>
+            </header>
+            <main></main>
+            <footer></footer>
+          </section>
+        </div>
       ) : (
         <div id={date} onClick={openModal} className="calendar__row--date">
           {date === 0 ? " " : date}
